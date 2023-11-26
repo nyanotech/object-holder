e1d43c818bac42e7f080df95550a6853eb00a2b7
nyanotech <nyanotechnology@gmail.com>
2023-11-26 04:40:07 -0800

    use api v2, drop region flag

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"sync"
 	"time"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 )
 
 var endpoint = flag.String("endpoint", "", "s3 endpoint")
-var region = flag.String("region", "", "s3 region")
 var bucket = flag.String("bucket", "", "bucket name")
 
 // todo - default to other credentials providers
@@ -28,36 +29,48 @@ var wg sync.WaitGroup
 func main() {
 	flag.Parse()
 
-	sess, err := session.NewSession(&aws.Config{
-		Region:      aws.String(*region),
-		Endpoint:    aws.String(*endpoint),
-		Credentials: credentials.NewStaticCredentials(*accessKeyId, *secretAccessKey, ""),
-	})
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		// just a placeholder because it needs to be defined
+		// should be using teh one from the endpoint regardless
+		config.WithRegion("us-east-1"),
+		config.WithEndpointResolver(aws.EndpointResolverFunc(
+			func(service, region string) (aws.Endpoint, error) {
+				return aws.Endpoint{
+					URL: *endpoint,
+				}, nil
+			},
+		)),
+		config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(*accessKeyId, *secretAccessKey, "")),
+	)
 	if err != nil {
-		log.Fatal("Failed to create AWS session:", err)
+		log.Fatal("Failed to create AWS config", err)
 	}
 
-	svc := s3.New(sess)
-	err = svc.ListObjectsV2Pages(&s3.ListObjectsV2Input{
+	svc := s3.NewFromConfig(cfg)
+
+	paginator := s3.NewListObjectsV2Paginator(svc, &s3.ListObjectsV2Input{
 		Bucket: bucket,
-	}, func(page *s3.ListObjectsV2Output, lastPage bool) bool {
+	})
+
+	for paginator.HasMorePages() {
+		page, err := paginator.NextPage(context.TODO())
+		if err != nil {
+			log.Fatal("Failed to list objects", err)
+		}
+
 		for _, item := range page.Contents {
 			wg.Add(1)
 			go checkAndRenewObjectLock(svc, *item.Key)
 		}
-
-		return !lastPage
-	})
-	if err != nil {
-		log.Fatal("Failed to list objects:", err)
 	}
 
 	wg.Wait()
 }
 
-func checkAndRenewObjectLock(svc *s3.S3, object string) {
+func checkAndRenewObjectLock(svc *s3.Client, object string) {
 	defer wg.Done()
-	retention, err := svc.GetObjectRetention(&s3.GetObjectRetentionInput{
+
+	retention, err := svc.GetObjectRetention(context.TODO(), &s3.GetObjectRetentionInput{
 		Bucket: bucket,
 		Key:    &object,
 	})
@@ -67,12 +80,12 @@ func checkAndRenewObjectLock(svc *s3.S3, object string) {
 
 	if retention.Retention.RetainUntilDate.Before(time.Now().Add(time.Second * time.Duration(*updateExpiresWithin))) {
 		log.Println("Renewing object lock for object", object)
-		_, err := svc.PutObjectRetention(&s3.PutObjectRetentionInput{
+		_, err := svc.PutObjectRetention(context.TODO(), &s3.PutObjectRetentionInput{
 			Bucket: bucket,
 			Key:    &object,
-			Retention: &s3.ObjectLockRetention{
-				Mode:            aws.String("COMPLIANCE"),
-				RetainUntilDate: aws.Time(time.Now().UTC().Add(time.Second * time.Duration(*lockFor))),
+			Retention: &types.ObjectLockRetention{
+				Mode:            "COMPLIANCE",
+				RetainUntilDate: aws.Time(time.Now().Add(time.Second * time.Duration(*lockFor))),
 			},
 		})
 		if err != nil {
